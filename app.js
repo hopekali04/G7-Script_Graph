aab7d2f3aa3e817185639fbee08b1974d5d15b6f
Hope Kalitera <hopekalitera@gmail.com>
2023-09-26 22:05:43 +0200

    feat: add rainfall, and index

--- app.js
+++ app.js
@@ -11,15 +11,41 @@ const expensesData = [
     { "Utilities": 80 }
 ];
 
-const home = (req, res) => {
+const rainfallData = [
+    {"January": 60.9},
+    {"February": 45.5},
+    {"March": 32.0},
+    {"April": 15.3},
+    {"May": 7.8},
+    {"June": 3.2},
+    {"July": 2.1},
+    {"August": 3.8},
+    {"September": 10.5},
+    {"October": 25.6},
+    {"November": 49.2},
+    {"December": 55.7}
+];  
+const home = (req, res) =>{
+    res.render("index")
+}
+const expense = (req, res) => {
     const data = {
-        title: "Home",
+        title: "expenses Graphs",
         expensesData: expensesData
     };
-    res.render("index", data);
+    res.render("expenses", data);
+};
+const rainfall = (req, res) => {
+    const data = {
+        title: "Rainfall Graphs",
+        rainfallData: rainfallData
+    };
+    res.render("rainfall", data);
 };
 
 app.get('/', home);
+app.get('/expense', expense);
+app.get('/rainfall', rainfall);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
